7a6f2c90e61f986cc81745008382ab099f7c91aa
alaanouassi <nouassi94@gmail.com>
2021-06-12 18:10:40 +0100

    Projet test resultat final

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'prenom' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string'
+        ], [
+            'name.required' => 'Veuillez entre votre nom',
+            'prenom.required' => 'Veuillez entre votre prenom',
+            'email.required' => 'Veuillez entre votre email',
+            'email.unique' => 'Adresse mail existe déjà',
+            'password.required' => 'Veuillez entre votre mot de passe'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'prenom' => $fields['prenom'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ], [
+            'email.required' => 'Veuillez Entrez tout les informations!',
+            'password.required' => 'Veuillez Entrez tout les informations!'
+        ]);
+
+        // check email
+        $user = User::where('email', $fields['email'])->first();
+        // check Password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Login ou mot de passe incorrecte!'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'prenom' => 'required|string',
+            'email' => 'required|string',
+        ], [
+            'name.required' => 'Veuillez entre votre nom',
+            'prenom.required' => 'Veuillez entre votre prenom',
+            'email.required' => 'Veuillez entre votre email',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->name = $fields['name'];
+        $user->prenom = $fields['prenom'];
+        $user->email = $fields['email'];
+        if ($request->password === null) {
+            $user->password =  $user->password;
+        } else {
+            $user->password =  bcrypt($request->password);
+        }
+        $user->civilite = $request->civilite;
+        $user->adresse = $request->adresse;
+        $user->ville = $request->ville;
+        $user->photo  = $request->photo;
+        // if ($request->hasFile('photo')) {
+        //     $path = $request->file('photo')->store('images');
+        //     $user->photo  = $path;
+        // }
+        $user->save();
+
+        return $user;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::put('/update/{id}', [AuthController::class, 'update']);
+});
